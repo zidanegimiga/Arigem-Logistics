be581ce995f33694629157c3f45eaf86fe17c7f1
zidanegimiga <zidanegimiga@gmail.com>
2022-07-09 17:41:56 +0300

    config: set up typography

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -23,10 +23,6 @@ class MyDocument extends Document {
             key="og-title"
           />
           <link rel="icon" href="/logo.png" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800;900&display=swap"
-            rel="stylesheet"
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
           <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@500;600;700;800;900&family=Manrope:wght@400;500;600;700;800&family=Marcellus+SC&display=swap" rel="stylesheet" />
