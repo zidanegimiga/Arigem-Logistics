6172d347a512c64153b70241a7ad6cef58b5281c
zidanegimiga <zidanegimiga@gmail.com>
2022-07-09 21:09:16 +0300

    feat: mount navbar onto the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 
+import Navbar from 'src/commons/Navbar';
+
+import styles from '../../styles/Home.module.scss';
+
 const Home =() => {
   return (
-    <div>
+    <div className={styles.index}>
       <Head>
         <title>Arigem Logistics</title>
       </Head>
-      This is the home page
+      <Navbar />
     </div>
   )
 }
