5844a4a939efbda65564c833c43eaf0bc148e060
zidanegimiga <zidanegimiga@gmail.com>
2022-07-09 21:08:38 +0300

    style: style navbar and drop-down items

--- src/commons/Navbar/Navbar.tsx
+++ src/commons/Navbar/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import NavbarDropDownItem from 'src/modules/NavbarDropDownItem';
+import Button from '../Button';
+import logo from '../../../public/logo.png';
+import styles from './Navbar.module.scss';
+import {COMPANY, SERVICES} from '../../utils/navbaritems';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className={styles.navWrapper}>
+      <div className={styles.logo}>
+        <Link href={"/"}>
+          <Image alt="Logo" src={logo} width={60} height={40}/>
+        </Link>
+        <span> ARIGEM LOGISTICS </span>
+      </div>
+      <NavbarDropDownItem title="SERVICES" items={SERVICES}/>
+      <NavbarDropDownItem title="COMPANY" items={COMPANY}/>
+      <div className={styles.navItem}>BLOG</div>
+      <Button />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
