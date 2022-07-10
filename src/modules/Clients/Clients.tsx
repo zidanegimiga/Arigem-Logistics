648821ccaac35db79a6316fe9bf2f07a6f4c3150
zidanegimiga <zidanegimiga@gmail.com>
2022-07-10 09:26:16 +0300

    feat: create clients section

---
+++ src/modules/Clients/Clients.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import hero from '../../../public/hero-image.png';
+import styles from './Clients.module.scss';
+import clientsLogos from '../../utils/clientsLogos';
+
+const Clients = () => {
+  return (
+    <div className={styles.sectionWrapper}> 
+      <h3 className={styles.sectionTitle}>Over 32 businesses trusted and are growing with ARIGEM</h3>
+      <div className={styles.imageWrapper}>
+        {
+          clientsLogos?.map((client) =>(
+            <div key={client.id}>
+              <Image src={client.path} alt={client.alt} width={client.width} height={client.height}/>
+            </div>
+          ))
+        }
+      </div>          
+    </div>
+  )
+}
+
+export default Clients;
---
+++ src/utils/clientsLogos.js
@@ -0,0 +1,46 @@
+const clientsLogos = [
+    {
+        id: 0,
+        path: "/CBK.png",
+        alt: "Central bank of kenya",
+        width: 106,
+        height: 82
+    },
+    {
+        id: 1,
+        path: "/ForeignAffairs.png",
+        alt: "Ministry of Foreign Affairs",
+        width: 332,
+        height: 82
+    },
+    {
+        id: 2,
+        path: "/IPOA.png",
+        alt: "IPOA",
+        width: 82,
+        height: 82
+    },
+    {
+        id: 3,
+        path: "/KICC.png",
+        alt: "K.I.C.C",
+        width: 115,
+        height: 82
+    },
+    {
+        id: 4,
+        path: "/TSC.png",
+        alt: "T.S.C",
+        width:173,
+        height: 82
+    },
+    {
+        id: 5,
+        path: "/WEF.png",
+        alt: "Women Enterprise Fund",
+        width: 176,
+        height: 82
+    },
+]
+
+export default clientsLogos;
