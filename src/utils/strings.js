c856ca5181a1fb601157d1db4609fa6d7e334d23
zidanegimiga <zidanegimiga@gmail.com>
2022-07-10 09:27:52 +0300

    style: style services card component

---
+++ src/utils/strings.js
@@ -0,0 +1,33 @@
+const SERVICEDESCRIPTION = "We are an office products based company. We provide office stationery, I.T equipment and furniture. We extend our service offering by including expertise in graphic design, brand strategization and web development.";
+
+const SERVICECARDS = [
+    {
+        color: "rgba(166, 206, 57, 0.32)",
+        number: "01",
+        title: "Office Stationery and I.T Equipment",
+        bodyText: "Boards & easel, Writing instrument, Paper, File organization, Desk Accessories, Punchers, Notebooks, Clip boards & Copy holders, Card scanners, Clips, Rubber band, Rulers, Calculators,",
+        image: "/stationery.png",
+        width: 244,
+        height: 171
+    },
+    {
+        color: "#EDD8D8",
+        number: "02",
+        title: "Office Furniture",
+        bodyText: "Security safes, fire resistant safes, Filing cabinets, Office chairs, Trolleys, Cupboards, Deposit safes, Office chair mat, Brochure & Catalogue stand, Key boxes, key accessories, Cash box.",
+        image: "/furniture.png",
+        width: 244,
+        height: 171
+    },
+    {
+        color: "#CDE2E9",
+        number: "03",
+        title: "Branding & Graphic Design",
+        bodyText: "Logo Design, Web Design, Brand Strategy, Calendars, stationery, Scarves, wall clocks, sweaters, jumpers, T-shirts, Flash Disks, Banners,Billboards.",
+        image: "/branding.png",
+        width: 244,
+        height: 171
+    },
+]
+
+export {SERVICEDESCRIPTION, SERVICECARDS};
