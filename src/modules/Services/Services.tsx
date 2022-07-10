b25de382239cdfdc6cd53cb3bc4e0860c889c896
zidanegimiga <zidanegimiga@gmail.com>
2022-07-10 13:19:16 +0300

    fix: format code with prettier

--- src/modules/Services/Services.tsx
+++ src/modules/Services/Services.tsx
@@ -8,9 +8,9 @@ const Services = () => {
   return (
     <div className={styles.sectionWrapper}>
       <div className={styles.description}>
-          <span>WHAT WE CAN DO FOR YOU</span>
-          <h2>We have a wide array of specialised services</h2>
-          <p>{SERVICEDESCRIPTION}</p>
+        <span>WHAT WE CAN DO FOR YOU</span>
+        <h2>We have a wide array of specialised services</h2>
+        <p>{SERVICEDESCRIPTION}</p>
       </div>
       <RoundedButton text="Talk to us" />
       <div className={styles.cardsWrapper}>
