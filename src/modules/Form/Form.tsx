9a37f088baea8aeb46ee56c99af5e6e63c2e3265
zidanegimiga <zidanegimiga@gmail.com>
2022-07-11 11:17:05 +0300

    feat: create form component

---
+++ src/modules/Form/Form.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Button from "src/commons/Button";
+import { Input, TextArea } from "../Form Input";
+import styles from './Form.module.scss';
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`${formData.name}, ${formData.email}, ${formData.message}`);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    console.log(formData);
+  };
+
+  return (
+    <div className={styles.form}>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.top}>
+          <Input
+            type="text"
+            label="Name"
+            placeholder="Enter your name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <Input
+            type="email"
+            label="E-mail"
+            placeholder="Email address"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.middle}>
+          <TextArea
+            label="Message"
+            placeholder="Type your message here"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.bottom}>
+          <Button type="submit" text="SEND MESSAGE"/>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
